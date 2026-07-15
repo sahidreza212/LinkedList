011f98508c52f05650ecd35c1e94b95d9ed02114
Sahid Reza <sahidreza976@gmail.com>
2026-07-15 12:53:49 +0530

    make every thing correct

--- src/main/java/org/example/LinkedList_Cycle.java
+++ src/main/java/org/example/LinkedList_Cycle.java
@@ -43,16 +43,13 @@ public class LinkedList_Cycle {
 
         LinkedList_Cycle ll = new LinkedList_Cycle();
 
-        // build list: A -> B -> C -> D
         LinkedList_Cycle.ListNode head = ll.new ListNode("A");
         head.next = ll.new ListNode("B");
         head.next.next = ll.new ListNode("C");
         head.next.next.next = ll.new ListNode("D");
 
-        // no cycle case
         System.out.println("Has cycle: " + ll.hasCyclic(head));
 
-        // create a cycle: D -> B (points back into the list)
         head.next.next.next.next = head.next;
         System.out.println("Has cycle: " + ll.hasCyclic(head));
     }
